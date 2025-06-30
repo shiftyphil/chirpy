6d148b81023d834cea561b1ccb0d420423e63a48
Philip Higgins <phil@nexvia.com.au>
2025-06-30 13:12:52 +1000

    CH2 - L1 - Add metrics.

--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+// API
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func healthHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
+}
+
+// Metrics
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) metricsResetHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
 	writer.WriteHeader(http.StatusOK)
 	writer.Write([]byte("OK"))
 }
@@ -20,10 +48,14 @@ func main() {
 		Addr:    ":8080",
 	}
 
+	apiCfg := apiConfig{}
+
 	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.metricsResetHandler)
 
 	fileHandler := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileHandler))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fileHandler)))
 
 	err = server.ListenAndServe()
 	if err != nil {
